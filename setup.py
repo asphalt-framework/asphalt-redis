06d6e8dd6110cc9f2ecb68a6ef39a1be0c6253cb
Alex Grönholm <alex.gronholm@nextday.fi>
2016-12-18 13:23:20 +0200

    Added tests and classifier for Python 3.6

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
         'Topic :: Database',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     license='Apache License 2.0',
     zip_safe=False,
